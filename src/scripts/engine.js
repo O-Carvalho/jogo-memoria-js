9b09b56963caa46f542181090207f74208bc9740
Matheus Carvalho <matheus.dev23@gmail.com>
2023-11-17 11:28:28 -0300

    Criando Card dinamicamente

---
+++ src/scripts/engine.js
@@ -0,0 +1,28 @@
+const emojis = [
+    "👽",
+    "👽",
+    "🪖",
+    "🪖",
+    "🫡",
+    "🫡",
+    "🥵",
+    "🥵",
+    "🤡",
+    "🤡",
+    "🧌",
+    "🧌",
+    "👨🏼‍💻",
+    "👨🏼‍💻"
+];
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5) ? 2 : -1);
+
+for(let i = 0; i < emojis.length; i++)
+{
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    document.querySelector(".game").
+    appendChild(box);
+}
